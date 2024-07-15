9bc36365cf760f1b7fda796e509f833804bd035f
musiienko25 <devmusiienko@gmail.com>
2024-07-15 17:26:05 +0300

    new object structure

--- src/components/bidDetails/BidDetails.js
+++ src/components/bidDetails/BidDetails.js
@@ -1,9 +1,12 @@
+// BidDetails.js
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./bidDetails.css";
 
 const BidDetails = ({ id }) => {
-  const [htmlContent, setHtmlContent] = useState("");
+  const [bidDetails, setBidDetails] = useState(null);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchBidDetails = async () => {
@@ -11,36 +14,56 @@ const BidDetails = ({ id }) => {
         const response = await axios.get(
           `http://localhost:5000/api/scrape?id=${id}`
         );
-        setHtmlContent(response.data);
+        setBidDetails(response.data);
       } catch (error) {
         console.error("Error fetching bid details:", error);
+        setError("Error fetching data. Check console for details.");
       }
     };
 
     fetchBidDetails();
   }, [id]);
 
+  if (error) {
+    return <div className="error-message">{error}</div>;
+  }
+
+  if (!bidDetails) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="bid-details">
-      <div className="html-content">
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Status</th>
-              <th>Due / Close Date</th>
-              <th>Publish Date UTC-4</th>
-              <th>Main Category</th>
-              <th>Solicitation Type</th>
-              <th>Issuing Agency</th>
-              <th>Bid Holders List</th>
-              <th>eMM ID</th>
-            </tr>
-          </thead>
-          <tbody dangerouslySetInnerHTML={{ __html: htmlContent }} />
-        </table>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Due / Close Date</th>
+            <th>Publish Date UTC-4</th>
+            <th>Main Category</th>
+            <th>Solicitation Type</th>
+            <th>Issuing Agency</th>
+            <th>Bid Holders List</th>
+            <th>eMM ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{bidDetails.id}</td>
+            <td>{bidDetails.title}</td>
+            <td>{bidDetails.status}</td>
+            <td>{bidDetails.dueDate}</td>
+            <td>{bidDetails.publishDate}</td>
+            <td>{bidDetails.mainCategory}</td>
+            <td>{bidDetails.solicitationType}</td>
+            <td>{bidDetails.issuingAgency}</td>
+            <td>{bidDetails.bidHoldersList}</td>
+            <td>{bidDetails.emmId}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
