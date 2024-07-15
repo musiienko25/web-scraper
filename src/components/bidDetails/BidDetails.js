6fb43e54ce879bdc50e91a98472309fbff97e3e6
musiienko25 <devmusiienko@gmail.com>
2024-07-15 15:02:25 +0300

    table data

--- src/components/bidDetails/BidDetails.js
+++ src/components/bidDetails/BidDetails.js
@@ -1,33 +1,33 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./bidDetails.css";
 
 const BidDetails = ({ id }) => {
   const [htmlContent, setHtmlContent] = useState("");
 
+  const fetchBidDetails = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/scrape?id=${id}`
+      );
+      setHtmlContent(response.data);
+    } catch (error) {
+      console.error("Error fetching bid details:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchBidDetails = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/api/scrape?id=${id}`
-        );
-        setHtmlContent(response.data);
-      } catch (error) {
-        console.error("Error fetching bid details:", error);
-      }
-    };
-
     fetchBidDetails();
-
-    return () => {};
   }, [id]);
 
   return (
     <div className="bid-details">
-      <div
-        className="html-content"
-        dangerouslySetInnerHTML={{ __html: htmlContent }}
-      />
+      <div className="html-content">
+        {/* Обгортка у <table> для збереження <td> тегів */}
+        <table>
+          <tbody dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        </table>
+      </div>
     </div>
   );
 };
